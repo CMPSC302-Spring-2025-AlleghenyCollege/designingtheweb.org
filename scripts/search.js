b43bb37c27ba1e5d0e14650b9795bb71816a89fe
CalebKendra <calebkendra7@gmail.com>
2025-04-23 10:49:49 -0400

    fixed jquery error

--- scripts/search.js
+++ scripts/search.js
@@ -1,33 +1,41 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Search functionality
-    document.getElementById('search-button').addEventListener('click', function() {
+document.addEventListener("DOMContentLoaded", function () {
+  // Search functionality
+  document
+    .getElementById("search-button")
+    .addEventListener("click", function () {
+      searchAndScroll();
+      console.log("yes");
+    });
+
+  document
+    .getElementById("search-input")
+    .addEventListener("keypress", function (e) {
+      if (e.key === "Enter") {
+        // Enter key pressed
         searchAndScroll();
+      }
     });
 
-    document.getElementById('search-input').addEventListener('keypress', function(e) {
-        if (e.key === 'Enter') { // Enter key pressed
-            searchAndScroll();
+  function searchAndScroll() {
+    var searchTerm = document
+      .getElementById("search-input")
+      .value.toLowerCase();
+    if (searchTerm) {
+      var found = false;
+      var divs = document.querySelectorAll("div");
+      divs.forEach(function (div) {
+        if (div.textContent.toLowerCase().includes(searchTerm)) {
+          window.scrollTo({
+            top: div.offsetTop,
+            behavior: "smooth",
+          });
+          found = true;
+          return false; // Break the loop
         }
-    });
-
-    function searchAndScroll() {
-        var searchTerm = document.getElementById('search-input').value.toLowerCase();
-        if (searchTerm) {
-            var found = false;
-            var divs = document.querySelectorAll('div');
-            divs.forEach(function(div) {
-                if (div.textContent.toLowerCase().includes(searchTerm)) {
-                    window.scrollTo({
-                        top: div.offsetTop,
-                        behavior: 'smooth'
-                    });
-                    found = true;
-                    return false; // Break the loop
-                }
-            });
-            if (!found) {
-                alert('No results found');
-            }
-        }
+      });
+      if (!found) {
+        alert("No results found");
+      }
     }
+  }
 });
